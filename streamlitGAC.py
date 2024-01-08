7ace43dc4f0e2134934fdcc5010b09d4b1d26f73
BrettOlmstead <brett.olmstead@datarobot.com>
2024-01-08 14:09:19 -0500

    Updating the app

--- streamlitGAC.py
+++ streamlitGAC.py
@@ -32,13 +32,35 @@ def getUNSDG(data):
     Your response should be formatted as a bulleted list using markdown.
     The goals should be listed in order of their relatedness with the project. 
     Each bullet should start with the SDG Goal number and name.
-    The SDG goal number and name should be typed in bold font, followed by an explanation of why you think the project is aligned to this goal. Reference the sub goals in cases where there is a clear connection with the subgoal and the project..
+    The SDG goal number and name should be typed in bold font, followed by an explanation of why you think the project is aligned to this goal. Reference the sub goals in cases where there is a clear connection with the subgoal and the project. Also note 
     
     """},
     {"role": "user", "content": str(data)}
     ]
     )
     return response.choices[0].message.content
+
+
+def getUNSDG2(prompt):
+    data = pd.DataFrame({"promptText": [prompt]})
+    API_URL = 'https://cfds-ccm-prod.orm.datarobot.com/predApi/v1.0/deployments/{deployment_id}/predictions'
+    API_KEY = os.environ["DATAROBOT_API_TOKEN"]
+    DATAROBOT_KEY = os.environ["DATAROBOT_KEY"]
+    deployment_id = '659c47ddae0a988d8d01d16f'
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer {}'.format(API_KEY),
+        'DataRobot-Key': DATAROBOT_KEY,
+    }
+    url = API_URL.format(deployment_id=deployment_id)
+    predictions_response = requests.post(
+        url,
+        data=data.to_json(orient='records'),
+        headers=headers
+    )
+
+    return predictions_response.json()["data"][0]["prediction"]
+
 
 def mainPage():
     with st.sidebar:
@@ -50,7 +72,7 @@ def mainPage():
     project = data.loc[data["Title"]==selected_project].set_index("Title").iloc[0]
     st.table(project)
     with st.spinner("processing..."):
-        response = getUNSDG(project)
+        response = getUNSDG2(project)
     st.subheader("Alignment to UN Sustainable Development Goals")
     st.write(response)
 
